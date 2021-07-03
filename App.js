547ca0a669fab52db221cfbb42608fe7b8379e96
Apiwat Srinukul <porplatakkom@Apiwats-MacBook-Pro.local>
2021-07-03 22:35:23 +0700

    [INIT] Create Project

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {Text, View} from "react-native";
+import HomeScreen from "./src/screen/HomeScreen";
+
+class App extends React.Component {
+  render(){
+    return (
+      <HomeScreen />
+    );
+  }
+
+}
+export default App;
---
+++ src/screen/HomeScreen.js
@@ -0,0 +1,5 @@
+import React, { Component } from "react";
+import { FlatList, Text, View } from "react-native";
+class HomeScreen extends Component{
+
+export default HomeScreen;
